07322874b0423817ecaa802507375f97fd5655a1
genialtechie <only1hbo@icloud.com>
2025-09-18 02:36:58 -0400

    fix schema lint error

--- studio/schemaTypes/mediaItem.ts
+++ studio/schemaTypes/mediaItem.ts
@@ -1,4 +1,8 @@
-import {defineType, defineField} from 'sanity'
+import {defineType, defineField, ValidationContext, type ImageRule, type UrlRule} from 'sanity'
+
+type Parent = {
+  mediaType?: 'image' | 'video'
+}
 
 export default defineType({
   name: 'mediaItem',
@@ -8,36 +12,50 @@ export default defineType({
     defineField({
       name: 'mediaType',
       title: 'Media Type',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       type: 'string',
       options: {
         list: [
           {title: 'Image', value: 'image'},
-          {title: 'Video', value: 'video'}
+          {title: 'Video', value: 'video'},
         ],
-        layout: 'radio'
-      }
+        layout: 'radio',
+      },
     }),
     defineField({
       name: 'image',
       title: 'Image',
-      validation: Rule => Rule.custom((value, {parent}) => parent.mediaType === 'image' && !value ? 'Image is required when Media Type is Image' : true),
       type: 'image',
-      hidden: ({parent}) => parent.mediaType !== 'image',
-      options: {hotspot: true}
+      options: {hotspot: true},
+      hidden: ({parent}: {parent: Parent | undefined}) => parent?.mediaType !== 'image',
+      validation: (Rule: ImageRule) =>
+        Rule.custom((value, context) => {
+          const {parent} = context as ValidationContext & {parent: Parent}
+          if (parent?.mediaType === 'image' && !value) {
+            return 'Image is required when Media Type is Image'
+          }
+          return true
+        }),
     }),
     defineField({
       name: 'videoUrl',
       title: 'Video URL',
-      validation: Rule => Rule.custom((value, {parent}) => parent.mediaType === 'video' && !value ? 'Video URL is required when Media Type is Video' : true),
       type: 'url',
-      hidden: ({parent}) => parent.mediaType !== 'video'
+      hidden: ({parent}: {parent: Parent | undefined}) => parent?.mediaType !== 'video',
+      validation: (Rule: UrlRule) =>
+        Rule.uri({}).custom((value, context) => {
+          const {parent} = context as ValidationContext & {parent: Parent}
+          if (parent?.mediaType === 'video' && !value) {
+            return 'Video URL is required when Media Type is Video'
+          }
+          return true
+        }),
     }),
     defineField({
       name: 'caption',
       title: 'Caption',
       type: 'string',
-      validation: Rule => Rule.required()
-    })
-  ]
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
 })
